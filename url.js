06d218297c130eb147768e76af7fc9aa5970ca9f
jingle1267 <jingle1267@163.com>
2019-12-06 14:20:07 +0800

    webp2png

---
+++ url.js
@@ -0,0 +1,8 @@
+// https://94275.cn/3dEye/images/panorama/1.png
+for (let i = 1; i <= 36; i++) {
+    console.log('https://94275.cn/3dEye/images/panorama/' + i + '.png')
+}
+// https://94275.cn/3dEye/images/hongda/1.png
+for (let i = 1; i <= 51; i++) {
+    console.log('https://94275.cn/3dEye/images/hongda/' + i + '.png')
+}
